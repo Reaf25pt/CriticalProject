3e22869fee49e8397566110414f62455fe0924f2
reaf25 <reaf25@gmail.com>
2023-05-31 09:40:07 +0100

    FR-profile

--- frontend/frontend/src/Pages/Profile.jsx
+++ frontend/frontend/src/Pages/Profile.jsx
@@ -1,8 +1,30 @@
+import { Col, Container, Image, Row } from "react-bootstrap";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+
 function Profile() {
   return (
-    <div>
-      <h3>Perfil</h3>
-    </div>
+    <Container>
+      <Row>
+        <Col md={3}>
+          <div>
+            <Image
+              src="https://randomuser.me/api/portraits/men/72.jpg"
+              roundedCircle
+            />
+            <p>Rodrigoferreira@gmail.com</p>
+            <p>Rodrigo Ferreira</p>
+            <p>Coimbra</p>
+            <p>
+              <BsFillEyeSlashFill />
+            </p>
+            <p>
+              <BsFillEyeFill />
+            </p>
+          </div>
+        </Col>
+        <Col md={9}>2</Col>
+      </Row>
+    </Container>
   );
 }
 
