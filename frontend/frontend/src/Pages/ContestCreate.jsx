c81578a39447f8d23517795c053e4952d65a1beb
reaf25 <reaf25@gmail.com>
2023-06-03 18:19:53 +0100

    Update ContestCreate.jsx

--- frontend/frontend/src/Pages/ContestCreate.jsx
+++ frontend/frontend/src/Pages/ContestCreate.jsx
@@ -7,42 +7,38 @@ import ButtonComponent from "../Components/ButtonComponent";
 
 function ContestCreate() {
   return (
-    <div>
-      <Row className="mb-5">
+    <Container fluid>
+      <Container fluid className="mb-5">
         <SecondTitle name={"Criar concurso"} />
-      </Row>
-      <Container fluid className="d-flex justify-content-center">
-        {" "}
-        <Row className="mb-5"></Row>
-        <Row>
-          <Form>
-            <Row className={style.boxform}>
-              <InputComponent placeholder={"Titulo*"} />
-              <Col>
-                <InputComponent placeholder={"Data Inicio*"} />
-              </Col>
-              <Col>
-                <InputComponent placeholder={"Data Fim*"} />
-              </Col>
-              <InputComponent placeholder={"Nº Max de Projetos*"} />
-            </Row>
-            <Row className={style.boxform}>
-              <Col md={8}>
-                <TextAreaComponent name={"Descrição"} />
-              </Col>
-              <Col md={4}>
-                <TextAreaComponent name={"Regras"} />
-              </Col>
-            </Row>
-            <Container fluid className="mb-5 w-25 p-3 d-flex ">
-              <Col>
-                <ButtonComponent name={"Adicionar"} />
-              </Col>
-            </Container>
-          </Form>
-        </Row>
+      </Container>{" "}
+      <Container className="d-flex justify-content-center">
+        <Form>
+          <Row className={style.boxform}>
+            <InputComponent placeholder={"Titulo*"} />
+            <Col>
+              <InputComponent placeholder={"Data Inicio*"} />
+            </Col>
+            <Col>
+              <InputComponent placeholder={"Data Fim*"} />
+            </Col>
+            <InputComponent placeholder={"Nº Max de Projetos*"} />
+          </Row>
+          <Row className={style.boxform}>
+            <Col md={8}>
+              <TextAreaComponent name={"Descrição"} />
+            </Col>
+            <Col md={4}>
+              <TextAreaComponent name={"Regras"} />
+            </Col>
+          </Row>
+          <Container fluid className="mb-5 w-25 p-3 d-flex ">
+            <Col>
+              <ButtonComponent name={"Adicionar"} />
+            </Col>
+          </Container>
+        </Form>
       </Container>
-    </div>
+    </Container>
   );
 }
 
