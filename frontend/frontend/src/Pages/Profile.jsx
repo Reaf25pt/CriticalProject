bf3ebd4c9f7245dd95ba83e1b95735dc0ec47ad0
reaf25 <reaf25@gmail.com>
2023-06-12 14:55:27 +0100

    tabela com ordenaçao no perfil

--- frontend/frontend/src/Pages/Profile.jsx
+++ frontend/frontend/src/Pages/Profile.jsx
@@ -1,8 +1,3 @@
-import { BsArrowDown, BsSearch } from "react-icons/bs";
-import ButtonComponent from "../Components/ButtonComponent";
-import LinkButton from "../Components/LinkButton";
-import TextAreaComponent from "../Components/TextAreaComponent";
-import SelectSkillType from "../Components/SelectSkillType";
 import ChangePasswordIn from "./ChangePasswordIn";
 import { userStore } from "../stores/UserStore";
 import { useEffect, useState } from "react";
@@ -10,6 +5,8 @@ import SeeProfileComponenent from "../Components/SeeProfileComponent";
 import EditProfileComponent from "../Components/EditProfileComponent";
 import Hobby from "../Components/Hobby";
 import Skill from "../Components/Skill";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 function Profile() {
   const user = userStore((state) => state.user);
@@ -18,6 +15,7 @@ function Profile() {
   const [credentials, setCredentials] = useState({});
   const userUpdate = userStore((state) => state.setUser);
   const [projects, setProjects] = useState([]);
+  const [showProjects, setShowProjects] = useState([]);
 
   const handleEdit = (event) => {
     setIsEditing(true);
@@ -85,14 +83,13 @@ function Profile() {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        token: user.tokenValue,
+        token: user.token,
       },
     })
       .then((resp) => resp.json())
       .then((data) => {
         console.log(data);
-        setProjects(data);
-        console.log(projects);
+        setShowProjects(data);
       })
       .catch((err) => console.log(err));
   }, [projects]);
@@ -148,62 +145,6 @@ function Profile() {
                   onClick={handleClick}
                 />
               )}
-              {/*   <div class="col-12 col-sm-12 col-md-12 col-lg-4 mt-3 ">
-                <div class="p-5 mb-4 bg-secondary h-100 rounded-5 ">
-                  <div class="text-center">
-                    {user.photo != null ? (
-                      <img
-                        src={user.photo}
-                        alt="avatar"
-                        class="rounded-circle img-responsive"
-                      />
-                    ) : (
-                      <img
-                        src="https://randomuser.me/api/portraits/women/22.jpg"
-                        alt="avatar"
-                        class="rounded-circle img-responsive"
-                      />
-                    )}
-                    {/*  <img
-                      src="https://randomuser.me/api/portraits/women/22.jpg"
-                      alt="avatar"
-                      class="rounded-circle img-responsive"
-                    /> */}
-              {/*      <h5 class="my-3 text-white">{user.email}</h5>
-                    <p class="text-white mb-1">
-                      {fullName} {user.nickname && `(${user.nickname})`}
-                    </p>
-                    <p class="text-white mb-4">{user.office}</p>
-                    {user.openProfile ? (
-                      <p class="text-white mb-4">Público</p>
-                    ) : (
-                      <p class="text-white mb-4">Privado</p>
-                    )}
-                    {/*                     <p class="text-white mb-4">Privado</p>
-                 /*     */}{" "}
-              {/*  <div class="d-flex justify-content-around">
-                      <ButtonComponent
-                        type="button"
-                        name="Editar"
-                        onClick={handleClick}
-                      />
-
-                      {/*   <LinkButton name="Alterar password" /> */}
-              {/*  </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-12 col-sm-12 col-md-12 col-lg-8 mt-3 ">
-                <div class="p-5 mb-4 bg-secondary h-100 rounded-5">
-                  <textarea
-                    class="text-dark bg-white h-75 w-100 rounded-2 mb-5"
-                    placeholder="Biografia..."
-                  ></textarea>
-                  <div className="row">
-                    <ButtonComponent name={"Editar"} />
-                  </div>
-                </div>
-              </div> */}
             </div>
             <div class="row mt-5">
               <div class="col-lg-4 ">
@@ -212,83 +153,37 @@ function Profile() {
                     <h3 className="bg-white text-center text-nowrap rounded-5 p-0  ">
                       Os meus Projetos:
                     </h3>
-                    <div className="text-white p-1 m-1 rounded-3 w-75 p-3 ">
-                      <div className="mb-2 bg-black rounded-3">
-                        <div>{projects.title}</div>
-                      </div>
-                      <div className="mb-2 bg-black rounded-3">
-                        <p>Projeto 2</p>
-                      </div>
-                    </div>
+                    <DataTable
+                      value={showProjects}
+                      tableStyle={{ minWidth: "25rem" }}
+                    >
+                      <Column
+                        field="title"
+                        header="Nome do Projeto"
+                        sortable
+                        style={{ width: "30%" }}
+                      ></Column>
+                      <Column
+                        field="status"
+                        header="Estado"
+                        sortable
+                        style={{ width: "10%" }}
+                      ></Column>
+                      <Column
+                        field="creationDate"
+                        header="Data do Registo"
+                        sortable
+                        style={{ width: "10%" }}
+                      ></Column>
+                    </DataTable>
                   </div>
                 </div>
               </div>
               <div class="col-lg-4 ">
                 <Skill />
-                {/* <div class="bg-secondary rounded-3 p-4 h-100">
-                  <div class="input-group rounded mb-3">
-                    <input
-                      type="search"
-                      class="form-control rounded "
-                      placeholder="Skills..."
-                      aria-label="Search"
-                      aria-describedby="search-addon"
-                    />
-                    <div class="form-group">
-                      <div class="input-group rounded">
-                        <SelectSkillType placeholder={"Categoria"} />
-                        <span
-                          class="input-group-text border-0"
-                          id="search-addon"
-                        >
-                          <BsArrowDown />
-                        </span>
-                      </div>
-                    </div>
-                    <span class="input-group-text border-0">
-                      <BsSearch />
-                    </span>
-                  </div>
-                  <div className="row mx-auto">
-                    <div className="w-25  bg-white m-1 rounded-3">
-                      <p>skills x</p>
-                    </div>
-                    <div className="w-25 m-1 bg-danger rounded-3">
-                      <p>skills x</p>
-                    </div>
-                    <div className="w-25 m-1 bg-danger rounded-3">
-                      <p>skills x</p>
-                    </div>
-                    <div className="w-25  bg-white m-1 rounded-3">
-                      <p>skills x</p>
-                    </div>
-                  </div>
-                </div> */}
               </div>
               <div class="col-lg-4 ">
                 <Hobby />
-                {/*  <div class=" bg-secondary rounded-3 p-4 h-100">
-                  <div class="input-group rounded mb-3">
-                    <input
-                      type="search"
-                      class="form-control rounded "
-                      placeholder="Hobbies"
-                      aria-label="Search"
-                      aria-describedby="search-addon"
-                    />
-                    <span class="input-group-text border-0">
-                      <BsSearch />
-                    </span>
-                  </div>
-                  <div className="d-flex">
-                    <div className="w-25  bg-white m-1 rounded-3">
-                      <p>Hobbies x</p>
-                    </div>
-                    <div className="w-25 m-1 bg-danger rounded-3">
-                      <p>Hobbies x</p>
-                    </div>
-                  </div>
-                </div> */}
               </div>
             </div>
           </div>
