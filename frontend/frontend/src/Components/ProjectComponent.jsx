92199487d4942a500cf354f2d6b251dc5735b7d1
reaf25 <reaf25@gmail.com>
2023-06-16 15:59:41 +0100

    apresentaçao do projeto

--- frontend/frontend/src/Components/ProjectComponent.jsx
+++ frontend/frontend/src/Components/ProjectComponent.jsx
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 
 function ProjectComponent() {
   const user = userStore((state) => state.user);
-  const [projects, setProjects] = useState([]);
   const [showProjects, setShowProjects] = useState([]);
 
   const { id } = useParams();
@@ -32,12 +31,13 @@ function ProjectComponent() {
         setShowProjects(data);
       })
       .catch((err) => console.log(err));
-  }, [projects]);
+  }, []);
+
   return (
     <div className="container-fluid">
       <div className="row mt-5">
         <div className="col-lg-4">
-          <div className="row bg-secondary rounded-5 p-4">
+          <div className="row bg-secondary rounded-5 p-4 mb-3">
             <div className="row p-3 mx-auto">
               <div className="col-lg-12 bg-white rounded-3 p-2">
                 {showProjects.title}
@@ -57,8 +57,38 @@ function ProjectComponent() {
               </select>
             </div>
           </div>
-          <div className="row"></div>
-          <div className="row"></div>
+          <div className="row bg-secondary rounded-5 p-4">
+            <div className="col-lg-12 bg-white rounded-5">
+              <h4 className="text-center">Palavras Chave</h4>
+            </div>
+            <div className="row mt-3 mx-auto">
+              {/* {keywords.map((keyword, index) => (
+                <div className="bg-danger rounded-5 " key={index}>
+                  <h5 className="text-white text-center"> {keyword.title} </h5>
+                </div>
+              ))} */}
+            </div>
+          </div>
+          <div className="row bg-secondary rounded-5 p-4">
+            <div className="col-lg-12 bg-white rounded-5">
+              <h4 className="text-center">Skill</h4>
+            </div>
+            {/* {showProjects.keywords[0]} */}
+            <div></div>
+
+            {/* {
+              <div className="row mt-3 mx-auto">
+                {showProjects.map((project, index) => (
+                  <div className="bg-danger rounded-5 " key={index}>
+                    <h5 className="text-white text-center">
+                      {" "}
+                      {project.title}{" "}
+                    </h5>
+                  </div>
+                ))}
+              </div>
+            } */}
+          </div>
         </div>
         <div className="col-8"></div>
       </div>
