1f78b5e2a6429f0150516a5c90f8b29eada8a3c7
reaf25 <reaf25@gmail.com>
2023-07-01 14:32:26 +0100

    Update SearchUser.jsx

--- frontend/frontend/src/Components/SearchUser.jsx
+++ frontend/frontend/src/Components/SearchUser.jsx
@@ -119,92 +119,82 @@ function SearchUser() {
       />
       <div className="dropdown bg-white">
         {suggestions &&
-          suggestions
-            /*  .filter((item) => {
-          return (
-            item && item.firstName.toLowerCase().includes(search)
-          );
-        })
-        .slice(0, 10) */
-            .map((item) => (
-              <option key={item.id} /* onClick={() => handleSelection(item)} */>
-                <div className="row d-flex justify-content-around p-2 ">
-                  <div className="col-lg-2 ">
-                    {item.photo === null ? (
-                      <img
-                        src="https://static-00.iconduck.com/assets.00/user-avatar-icon-512x512-vufpcmdn.png"
-                        width={30}
-                        height={30}
-                        alt=""
-                      />
-                    ) : (
-                      <img
-                        src={item.photo}
-                        width={35}
-                        height={35}
-                        className="rounded-5"
-                        alt=""
-                      />
-                    )}
-                  </div>
+          suggestions.map((item) => (
+            <option key={item.id}>
+              <div className="row d-flex justify-content-around p-2 ">
+                <div className="col-lg-2 ">
+                  {item.photo === null ? (
+                    <img
+                      src="https://static-00.iconduck.com/assets.00/user-avatar-icon-512x512-vufpcmdn.png"
+                      width={30}
+                      height={30}
+                      alt=""
+                    />
+                  ) : (
+                    <img
+                      src={item.photo}
+                      width={35}
+                      height={35}
+                      className="rounded-5"
+                      alt=""
+                    />
+                  )}
+                </div>
 
-                  <div className="col-lg-5">
-                    {item.firstName}
-                    {emptyStr}
-                    {item.lastName}
-                  </div>
-                  <div className="col-lg-1">
-                    {!item.openProfile ? (
+                <div className="col-lg-5">
+                  {item.firstName}
+                  {emptyStr}
+                  {item.lastName}
+                </div>
+                <div className="col-lg-1">
+                  {!item.openProfile ? (
+                    <OverlayTrigger
+                      placement="top"
+                      overlay={<Tooltip>Perfil privado</Tooltip>}
+                    >
+                      <span data-bs-toggle="tooltip" data-bs-placement="top">
+                        {" "}
+                        <BsEyeSlashFill size={25} />
+                      </span>
+                    </OverlayTrigger>
+                  ) : (
+                    <Link to={`/home/profile/${item.id}`}>
                       <OverlayTrigger
                         placement="top"
-                        overlay={<Tooltip>Perfil privado</Tooltip>}
+                        overlay={<Tooltip>Ver perfil</Tooltip>}
                       >
                         <span data-bs-toggle="tooltip" data-bs-placement="top">
                           {" "}
-                          <BsEyeSlashFill size={25} />
-                        </span>
-                      </OverlayTrigger>
-                    ) : (
-                      <Link to={`/home/profile/${item.id}`}>
-                        <OverlayTrigger
-                          placement="top"
-                          overlay={<Tooltip>Ver perfil</Tooltip>}
-                        >
-                          <span
-                            data-bs-toggle="tooltip"
-                            data-bs-placement="top"
-                          >
-                            {" "}
-                            <BsEyeFill
-                              size={25}
-                              color="black"
-                              //onClick={() => handleSeeProfile(item.id)}
-                            />
-                          </span>
-                        </OverlayTrigger>
-                      </Link>
-                    )}
-                  </div>
-                  <div className="col-lg-1">
-                    <Link>
-                      {" "}
-                      <OverlayTrigger
-                        placement="top"
-                        overlay={<Tooltip>Enviar mensagem</Tooltip>}
-                      >
-                        <span data-bs-toggle="tooltip" data-bs-placement="top">
-                          {" "}
-                          <BsMessenger
+                          <BsEyeFill
+                            size={25}
                             color="black"
-                            onClick={() => handleSendMessage(item.id)}
+                            //onClick={() => handleSeeProfile(item.id)}
                           />
                         </span>
                       </OverlayTrigger>
                     </Link>
-                  </div>
+                  )}
                 </div>
-              </option>
-            ))}
+                <div className="col-lg-1">
+                  <Link to={"/home/chat"}>
+                    {" "}
+                    <OverlayTrigger
+                      placement="top"
+                      overlay={<Tooltip>Enviar mensagem</Tooltip>}
+                    >
+                      <span data-bs-toggle="tooltip" data-bs-placement="top">
+                        {" "}
+                        <BsMessenger
+                          color="black"
+                          onClick={() => handleSendMessage(item.id)}
+                        />
+                      </span>
+                    </OverlayTrigger>
+                  </Link>
+                </div>
+              </div>
+            </option>
+          ))}
       </div>
     </div>
   );
